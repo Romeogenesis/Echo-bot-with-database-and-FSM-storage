20da6d362753f6dc21c16119b0b2def3fe363b5a
r.shpakovskiy <@r.shpakovskiy>
2025-06-29 19:16:21 +0300

    first commit

---
+++ migrations/create_tables.py
@@ -0,0 +1,22 @@
+import asyncio
+import logging
+import os
+import sys
+
+from config.config import Config, load_config
+from psycopg import AsyncConnection, Error
+
+config: Config = load_config()
+
+logging.basicConfig(
+    level=logging.getLevelName(level=config.log.level),
+    format=config.log.format
+)
+
+logger = logging.getLogger(__name__)
+
+if sys.platform.startswith("win") or os.name == "nt":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+async def main():
+    connection = As
